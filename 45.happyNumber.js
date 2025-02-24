fb667eb869eeb528b2d04418da107e5d9294a99a
GunjalArpita <gunjalarpita80@gmail.com>
2025-02-24 20:49:05 +0530

    Day 37 : Solved Happy Number Problem

---
+++ 45.happyNumber.js
@@ -0,0 +1,36 @@
+var isHappy = function(n)
+ {
+
+    let set = new Set();
+    for(let i = 0; i < 100; i++)
+    {
+        if(n === 1)
+            return true;
+        if(set.has(n))
+            return false;
+        set.add(n);
+        n = n.toString().split('').reduce((acc, cur) => acc + Math.pow(cur, 2), 0);
+    }
+    return false;
+
+
+};
+console.log(isHappy(19)); //true
+console.log(isHappy(2)); //false
+
+// Write an algorithm to determine if a number n is happy.
+// A happy number is a number defined by the following process:
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+// Example 1: Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+// Example 2:Input: n = 2 ,Output: false
